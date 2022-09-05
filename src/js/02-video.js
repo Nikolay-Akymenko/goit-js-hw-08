ff24b901a83d8ad55439f4780fd06ab78f42bc3b
Nikolay-Akymenko <nikolay0939@gmail.com>
2022-09-05 23:38:55 +0300

    Задание 2 , 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,40 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCLSTORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(checkTime, 1000));
+
+// Сохраняем в хранилище
+
+function checkTime(e) {
+    const message = JSON.stringify(e);
+    localStorage.setItem(LOCLSTORAGE_KEY, message);
+}
+
+const time = localStorage.getItem(LOCLSTORAGE_KEY);
+
+if (time !== null) {
+    const currentTime = JSON.parse(time);
+    const seconds = currentTime.seconds;
+
+player
+    .setCurrentTime(seconds)
+    .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(onInputForm), 500);
+form.addEventListener('submit', onSubmitForm);
+
+let storageForm = { email: '', message: '' };
+let loadingStorageForm = {};
+
+initForm();
+
+function initForm() {
+  let savedStorageForm = localStorage.getItem(
+    'feedback-form-state',
+    storageForm
+  );
+
+  if (savedStorageForm !== null) {
+    loadingStorageForm = JSON.parse(savedStorageForm);
+  } else {
+    loadingStorageForm = { email: '', message: '' };
+  }
+
+  form.elements.email.value = loadingStorageForm.email;
+  form.elements.message.value = loadingStorageForm.message;
+}
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  console.log(
+    ` email : ${form.elements.email.value}  message : ${form.elements.message.value}`
+  );
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+}
+
+function populateForm() {
+  localStorage.setItem('feedback-form-state', JSON.stringify(storageForm));
+}
+
+function onInputForm(event) {
+  storageForm.email = event.target.form.elements.email.value;
+  storageForm.message = event.target.form.elements.message.value;
+  populateForm();
+}
